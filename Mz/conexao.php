16ab5090f62f44595f108384ad08cd671d8e19d4
Diogeneslimax <113135011+Diogeneslimax@users.noreply.github.com>
2022-09-28 13:24:52 -0300

    delete mz

--- Mz/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost:3306";
-    $usuario = "root";
-    $senha = "O1X0il345!w-!Ly@";
-    $dbname = "chat-bot";
-    //Criar a conexao
-    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
